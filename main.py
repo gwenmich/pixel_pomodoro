32db1b5e20a6e62db77aad8c6aba395885af1f59
Gwen Michailidis <79330147+gwenmich@users.noreply.github.com>
2026-06-22 22:05:38 +0100

    Add title &  animated background

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from PySide6.QtWidgets import QApplication, QWidget
-
 import sys
+from PySide6.QtWidgets import QApplication, QMainWindow, QLabel
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QMovie
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Dead Focused")
+        self.setFixedSize(QSize(512, 700))
+
+        self.background = QMovie("background.gif")
+        self.label = QLabel("background")
+        self.setCentralWidget(self.label)
+        self.label.setScaledContents(True)
+        self.label.setFixedSize(QSize(512, 512))
+        self.label.setMovie(self.background)
+        self.background.start()
+        self.label.show()
+
+
+
 
 app = QApplication(sys.argv)
 
-window = QWidget()
-
+window = MainWindow()
 window.show()
 
 
